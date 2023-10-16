ed6e6bb79b7cd1dd997ea39d339b813f7790f003
LuisG <luis_halo_1999@live.com.mx>
2023-10-16 15:55:25 -0600

    versionMásFuncional

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
-const {isLoggedIn} = require('../lib/auth');
+const {isLoggedIn, isNotLoggedIn} = require('../lib/auth');
 
 
-router.get('/signup', (req, res)=>{
+router.get('/signup', isNotLoggedIn, (req, res)=>{
     res.render('auth/signup');
 })
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local.signup', {
         successRedirect: '/profile',
         failureRedirect: '/signup',
@@ -18,11 +18,11 @@ router.post('/signup', (req, res, next) => {
 });
 
 
-router.get('/signin', (req,res)=>{
+router.get('/signin', isNotLoggedIn, (req,res)=>{
     res.render('auth/signin');
 });
 
-router.post('/signin', (req, res, next)=>{
+router.post('/signin', isNotLoggedIn, (req, res, next)=>{
     passport.authenticate('local.signin', {
         successRedirect: '/profile',
         failureRedirect: '/signin'
