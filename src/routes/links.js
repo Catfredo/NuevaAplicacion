5b84c8e59c95c4933c30da67a93f7e27d14624f7
Rafael Flores <rafael-alam@hotmail.com>
2023-10-16 17:44:48 -0600

    Casi funciona

--- src/routes/links.js
+++ src/routes/links.js
@@ -31,29 +31,29 @@ router.get('/', isLoggedIn, async (req, res)=>{
     res.render('links/list', {Notas :  Notas})
 });
 
-router.get('/delete/:id_task',isLoggedIn, async (req, res)=>{
-    const { id_task } = req.params;
-    await pool.query('DELETE FROM task WHERE id_task = ?', [id_task]);
+router.get('/delete/:Id_task',isLoggedIn, async (req, res)=>{
+    const { Id_task } = req.params;
+    await pool.query('DELETE FROM task WHERE Id_task = ?', [Id_task]);
     //req.flash('success', 'Removido exitosamente');
     res.redirect('/links')
 });
 
-router.get('/edit/:id_task', isLoggedIn, async (req, res)=>{
-    const { id_task } = req.params;
-    const Notas = await pool.query('SELECT * FROM task WHERE id_task = ?', [id_task]);
+router.get('/edit/:Id_task', isLoggedIn, async (req, res)=>{
+    const { Id_task } = req.params;
+    const Notas = await pool.query('SELECT * FROM task WHERE Id_task = ?', [Id_task]);
     
     res.render('links/edit', {Notas: Notas[0]} );
 });
 
-router.post('/edit/:id_task', isLoggedIn, async(req, res)=>{
-    const {id_task } = req.params;
+router.post('/edit/:Id_task', isLoggedIn, async(req, res)=>{
+    const {Id_task } = req.params;
     const { taskname, body_task, duedate} = req.body;
     const newNota ={
         taskname,
         body_task,
         duedate,
     };
-    await pool.query('UPDATE task set ? WHERE id_task = ?', [newNota, id_task]);
+    await pool.query('UPDATE task set ? WHERE Id_task = ?', [newNota, Id_task]);
    // req.flash('success', 'Edición completada')
     res.redirect('/links');
 })
