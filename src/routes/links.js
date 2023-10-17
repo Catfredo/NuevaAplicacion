7b63260069a0a90bf0ce8b3bba265e9a1925e032
Rafael Flores <rafael-alam@hotmail.com>
2023-10-17 09:37:06 -0600

    Centro del nombre

--- src/routes/links.js
+++ src/routes/links.js
@@ -50,16 +50,10 @@ router.get('/edit/:Id_task', isLoggedIn, async (req, res)=>{
     res.render('links/edit', {Notas: Notas[0]} );
 });
 
-router.post('/edit/:Id_task', isLoggedIn, async(req, res)=>{
-    const {Id_task } = req.params;
-    const { taskname, body_task, duedate} = req.body;
-    const newNota ={
-        taskname,
-        body_task,
-        duedate,
-    };
-    await pool.query('UPDATE task set ? WHERE Id_task = ?', [newNota, Id_task]);
-   // req.flash('success', 'Edición completada')
+router.post('/edit/:Id_task', isLoggedIn, async (req, res) => {     const { Id_task } = req.params;     const { taskname, body_task, duedate } = req.body;  
+const currentDate = duedate ? new Date(duedate) : new Date();     const updatedNota = {taskname, body_task, duedate:currentDate, 
+    };     await pool.query('UPDATE task SET ? WHERE Id_task = ?', [updatedNota, Id_task]);
+    
     res.redirect('/links');
 })
 
